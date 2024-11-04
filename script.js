9d48a3482c30e8557ccbac3f7b81991fc2bfad78
Thiago <thiiagoo106@gmail.com>
2024-11-04 16:14:57 -0300

    Aplicação Section Especialidades Fotograficas

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navigationButtton = document.querySelector('.navigation-buttons');
-
-hamburgerMenu,addEventListener('click', ()=> {
+const navigationButtton = document.querySelectorAll('.navigation-buttons');
+const photos = document.querySelectorAll('.photos'); 
+hamburgerMenu.addEventListener('click', () => {
       hamburgerMenu.classList.toggle("activated");
       navigationButtton.classList.toggle("side");
 })
+
+photos.forEach(photo => {
+      photo.addEventListener('mouseenter', () => {
+            photos.forEach(el => {
+                  if (el !== photo) {
+                        el.classList.add('preto-e-branco'); // Aplica a classe de preto e branco
+                  }
+            });
+      });
+
+      photo.addEventListener('mouseleave', () => {
+            photos.forEach(el => {
+                  el.classList.remove('preto-e-branco'); // Remove a classe de preto e branco
+            });
+      });
+});
