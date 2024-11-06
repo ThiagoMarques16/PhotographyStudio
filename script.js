0009d45c0824a33676b76ba4964bceec276177e6
Thiago <thiiagoo106@gmail.com>
2024-11-06 16:05:13 -0300

    Alteração Home

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navigationButtton = document.querySelectorAll('.navigation-buttons');
-const photos = document.querySelectorAll('.photos'); 
+const navigationButtons = document.querySelectorAll('.navigation-buttons');
+const photos = document.querySelectorAll('.photos');
+const textElement = document.querySelector('.text-info');
+
 
 
 hamburgerMenu.addEventListener('click', () => {
-      hamburgerMenu.classList.toggle("activated");
-      navigationButtton.classList.toggle("side");
-})
+      hamburgerMenu.classList.toggle('activated');
+
+      // Aplica a classe 'side' a todos os elementos de 'navigation-buttons'
+      navigationButtons.forEach(button => {
+            button.classList.toggle('side');
+      });
+});
 
 photos.forEach(photo => {
       photo.addEventListener('mouseenter', () => {
@@ -19,7 +25,22 @@ photos.forEach(photo => {
 
       photo.addEventListener('mouseleave', () => {
             photos.forEach(el => {
-                  el.classList.remove('preto-e-branco'); 
+                  el.classList.remove('preto-e-branco');
             });
       });
 });
+
+function activateFadeIn(entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('fade-in');  // Aplica a animação de fade in
+              observer.unobserve(entry.target); // Para evitar que o efeito seja reativado
+          }
+      });
+  }
+
+  const observer = new IntersectionObserver(activateFadeIn, {
+      threshold: 0.5  // Ativa o efeito quando 50% do elemento estiver visível
+  });
+
+observer.observe(textElement);
