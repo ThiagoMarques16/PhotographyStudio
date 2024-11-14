96f932322a52d8899f24961c354ffd5bd74b1ad7
Thiago <thiiagoo106@gmail.com>
2024-11-14 10:11:16 -0300

    Atualização Album

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navigationButtons = document.querySelectorAll('.navigation-buttons');
-const photos = document.querySelectorAll('.photos');
 const textElement = document.querySelector('.text-info');
 
 
 
 hamburgerMenu.addEventListener('click', () => {
-      hamburgerMenu.classList.toggle('activated');
+    hamburgerMenu.classList.toggle('activated');
 
-     
-      navigationButtons.forEach(button => {
-            button.classList.toggle('side');
-      });
+
+    navigationButtons.forEach(button => {
+        button.classList.toggle('side');
+    });
 });
 
-photos.forEach(photo => {
-      photo.addEventListener('mouseenter', () => {
-            photos.forEach(el => {
-                  if (el !== photo) {
-                        el.classList.add('preto-e-branco');
-                  }
-            });
-      });
-
-      photo.addEventListener('mouseleave', () => {
-            photos.forEach(el => {
-                  el.classList.remove('preto-e-branco');
-            });
-      });
-});
 
 function activateFadeIn(entries, observer) {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              entry.target.classList.add('fade-in'); 
-              observer.unobserve(entry.target); 
-          }
-      });
-  }
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}
 
-  const observer = new IntersectionObserver(activateFadeIn, {
-      threshold: 0.5  
-  });
+const observer = new IntersectionObserver(activateFadeIn, {
+    threshold: 0.5
+});
 
 observer.observe(textElement);
+
+window.addEventListener('DOMContentLoaded', function () {
+    const overlay = document.querySelector('.overlay');
+    const albumContainer = document.querySelector('.album-container');
+
+    setTimeout(function () {
+        overlay.style.opacity = '0';
+        albumContainer.style.opacity = '1';
+    }, 2500);
+});
