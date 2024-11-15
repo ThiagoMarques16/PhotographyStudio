8e2b23cab09d2d1a813a80676f105cb1c071feda
Thiago Marques <thiiagoo106@gmail.com>
2024-11-15 13:21:21 -0300

    Responsividade contato

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navigationButtons = document.querySelectorAll('.navigation-buttons');
 const textElement = document.querySelector('.text-info');
+const btnEnviar = document.querySelector('#enviar');
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+const telefone = document.querySelector('#telefone');
+const mensagem = document.querySelector('#mensagem');
+const error = document.querySelector('.error');
 
 
 
+//Ativação menu hamburguer
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('activated');
 
@@ -14,6 +21,18 @@ hamburgerMenu.addEventListener('click', () => {
 });
 
 
+//Verificação valores do formulario e envio de formulario
+btnEnviar.addEventListener("click", () => {
+    event.preventDefault();
+    if (nome.value.trim() !== "" && email.value.trim() !== "" && telefone.value.trim() !== "") {
+        alert(`Obrigado ${nome.value} entraremos em contato`);
+        error.classList.remove('info')
+    } else {
+        error.classList.add('info');
+    }
+});
+
+// Efeito fade in do texto do home
 function activateFadeIn(entries, observer) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -29,29 +48,27 @@ const observer = new IntersectionObserver(activateFadeIn, {
 
 observer.observe(textElement);
 
+//Efeito overlay da galeria
 window.addEventListener('DOMContentLoaded', function () {
     const overlay = document.querySelector('.overlay');
     const albumContainer = document.querySelector('.album-container');
 
-    // Função que vai ser chamada quando o alvo (galeria) estiver visível
     function handleIntersection(entries, observer) {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 setTimeout(function () {
-                    overlay.style.opacity = '0';  
-                    albumContainer.style.opacity = '1'; 
-                }, 2500);  
+                    overlay.style.opacity = '0';
+                    albumContainer.style.opacity = '1';
+                }, 2500);
                 observer.unobserve(entry.target);
             }
         });
     }
 
-    // Criando o observer para observar a galeria
     const observer = new IntersectionObserver(handleIntersection, {
-        threshold: 0.2  // A animação acontece quando pelo menos 50% da galeria estiver visível
+        threshold: 0.2
     });
 
-    // Observe a seção da galeria
     const albumSection = document.querySelector('.album-section');
     observer.observe(albumSection);
 });
